a37e41a70c28263d7e008fba4c5454aa00372fed
LeonidasAsprilla <leonidasa@uninorte.edu.co>
2022-07-04 20:46:53 -0500

    Roles Usuario

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
 
-Route::group(["prefix" => "dashboard", "middleware" => "auth"], function (){
+Route::group(["prefix" => "dashboard", "middleware" => ["auth", "admin"]], function (){
     Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
